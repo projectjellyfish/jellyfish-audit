03f50008a280a0eea9e3930738e63b5e7ffcbe45
mafernando <fernando_michael@bah.com>
2015-04-21 13:02:19 -0500

    added primitive log to file capability updated readme with steps to setup dotenv for log to file. added write_to_file and extract_payload to audit_client.

--- README.md
+++ README.md
@@ -9,7 +9,24 @@ Include in Gemfile:
 ```
 or download from GitHub and reference locally:
 ```
-gem 'jellyfish-audit', path: '../jellyfish-audit'
+  gem 'jellyfish-audit', path: '../jellyfish-audit'
 ```
-
+then update `.env` with:
+```
+  LOG_TO_FILE = true
+```
+If `LOG_TO_FILE` is set to `true` then API requests will be logged to `/log/audit.txt`: 
+```
+Controller: SessionsController
+Method: POST
+Action: create
+Format: json
+Params: {"staff"=>{"email"=>"admin@projectjellyfish.org", "password"=>"[FILTERED]"}, "controller"=>"sessions", "action"=>"create", "format"=>"json"}
+Controller: AlertsController
+Method: GET
+Action: index
+Format: json
+Params: {"latest"=>"true", "format"=>:json, "controller"=>"alerts", "action"=>"index"}
+...
+```
 This project uses APACHE LICENSE.
--- lib/jellyfish_audit/audit_client.rb
+++ lib/jellyfish_audit/audit_client.rb
@@ -1,16 +1,31 @@
 module JellyfishAudit
   class AuditClient < ActionController::LogSubscriber
     def process_action(event)
-      payload = event.payload
+      # TODO: SURROUND WITH TRY CATCH MAYBE?
+
+      extract_payload(event)
+
+      log_payload
+
+      prepare_content
+
+      write_to_file if ENV['LOG_TO_FILE'] == 'true'
+    end
+
+    private
+
+    def extract_payload(event)
+      @payload = event.payload
+      @controller = @payload[:controller]
+      @action = @payload[:action]
+      @params = @payload[:params]
+      @format = @payload[:format]
+      @method = @payload[:method]
+      @path = @payload[:path]
+    end
+
+    def log_payload
       Rails.logger.info('START RAW ACTION')
-
-      @controller = payload[:controller]
-      @action = payload[:action]
-      @params = payload[:params]
-      @format = payload[:format]
-      @method = payload[:method]
-      @path = payload[:path]
-
       Rails.logger.info('Controller: ' + @controller)
       Rails.logger.info('Method: ' + @method)
       Rails.logger.info('Action: ' + @action)
@@ -18,6 +33,25 @@ module JellyfishAudit
       Rails.logger.info('Path: ' + @path)
       Rails.logger.info('Params: ' + @params.to_s)
       Rails.logger.info('END RAW ACTION')
+    end
+
+    def prepare_content
+      content = []
+      content << 'Controller: ' + @controller
+      content << 'Method: ' + @method
+      content << 'Action: ' + @action
+      content << 'Format: ' + @format.to_s
+      content << 'Params: ' + @params.to_s
+      @content = content.map { |ln| "#{ln}\n" }.join
+    end
+
+    def write_to_file
+      path = Rails.root.to_s + '/log/audit.txt'
+      Rails.logger.info('START LOGGING TO: ' + path)
+      File.open(path, 'a') do |f|
+        f.puts(@content)
+      end
+      Rails.logger.info('END LOGGING TO: ' + path)
     end
   end
 end
