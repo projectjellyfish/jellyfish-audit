1bc0c7d7de5c23e39be35c00d4a28912981dff36
mafernando <fernando_michael@bah.com>
2015-04-21 21:30:06 -0500

    change where audit client is loaded from

--- lib/jellyfish_audit.rb
+++ lib/jellyfish_audit.rb
@@ -1,4 +1,5 @@
 require 'jellyfish_audit/engine'
+require 'jellyfish_audit/audit_client'
 
 module JellyfishAudit
 end
--- lib/jellyfish_audit/engine.rb
+++ lib/jellyfish_audit/engine.rb
@@ -1,6 +1,5 @@
 module JellyfishAudit
   class Engine < ::Rails::Engine
-    config.autoload_paths += %W(#{config.root}/lib)
     isolate_namespace JellyfishAudit
   end
 end
